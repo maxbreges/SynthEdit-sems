76737225ce40114bd557fcf39287387b657a3eda
maxbreges <mbreges@gmail.com>
2024-08-18 13:46:14 +0300

    an issue

--- PatchMemKnob/PatchMemKnob.cpp
+++ PatchMemKnob/PatchMemKnob.cpp
@@ -1,9 +1,7 @@
 #include "mp_sdk_gui2.h"
 #include "Drawing.h"
-#include <iomanip>
-#include <sstream>
 #define _USE_MATH_DEFINES
-
+#include "C:\Program Files (x86)\Windows Kits\10\Include\10.0.10150.0\ucrt\math.h"
 
 using namespace gmpi;
 using namespace gmpi_gui;
@@ -11,7 +9,7 @@ using namespace GmpiDrawing;
 
 GmpiDrawing_API::MP1_POINT pointPrevious;
 
-class WirelessPatchMemGui final : public gmpi_gui::MpGuiGfxBase
+class PatchMemKnobGui final : public gmpi_gui::MpGuiGfxBase
 {
 	void onSetScaleIn()
 	{
@@ -28,25 +26,13 @@ class WirelessPatchMemGui final : public gmpi_gui::MpGuiGfxBase
 	void onSetAnimationPosition()
 	{
 		pinAnimationIn = pinAnimationPosition;
-		if (pinColorAdj)
-		{
-			AnimPosToHex();
-			pinHex = pinHexIn;
-		}
-		invalidateRect();
-		if (!pinColorAdj)
-			pinHexIn = pinHex;
 	}
-
 	void onSetHexIn()
 	{
-		//pinHex = pinHexIn;
 	}
 	void onSetHex()
 	{
-		//pinHexIn = pinHex;
 	}
-
 
 public:
  	FloatGuiPin pinScaleIn;
@@ -54,19 +40,17 @@ public:
  	FloatGuiPin pinScaled;
  	FloatGuiPin pinAnimationPosition;
 	StringGuiPin pinHexIn;
-	BoolGuiPin pinColorAdj;
 	StringGuiPin pinHex;
 
 
-	WirelessPatchMemGui()
+	PatchMemKnobGui()
 	{
-		initializePin( pinScaleIn, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetScaleIn) );
-		initializePin( pinAnimationIn, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetAnimationIn) );
-		initializePin( pinScaled, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetScaled) );
-		initializePin( pinAnimationPosition, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetAnimationPosition) );
-		initializePin(pinHexIn, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetHexIn));
-		initializePin(pinColorAdj);
-		initializePin(pinHex, static_cast<MpGuiBaseMemberPtr2>(&WirelessPatchMemGui::onSetHex));
+		initializePin( pinScaleIn, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetScaleIn) );
+		initializePin( pinAnimationIn, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetAnimationIn) );
+		initializePin( pinScaled, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetScaled) );
+		initializePin( pinAnimationPosition, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetAnimationPosition) );
+		initializePin(pinHexIn, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetHexIn));
+		initializePin(pinHex, static_cast<MpGuiBaseMemberPtr2>(&PatchMemKnobGui::onSetHex));
 	}
 
 	int32_t MP_STDCALL onPointerDown(int32_t flags, GmpiDrawing_API::MP1_POINT point) override
@@ -90,9 +74,10 @@ public:
 		{
 			return gmpi::MP_UNHANDLED;
 		}
-		releaseCapture();
-		return gmpi::MP_OK;
 
+		releaseCapture();
+
+		return gmpi::MP_OK;
 	}
 
 	int32_t MP_STDCALL onPointerMove(int32_t flags, GmpiDrawing_API::MP1_POINT point) override
@@ -128,90 +113,13 @@ public:
 		return gmpi::MP_OK;
 	}
 
-	int32_t AnimPosToHex()
-	{
-		int x = pinAnimationPosition * 1535;
-		int R = 0;
-		int G = 0;
-		int B = 0;
-		//--------------------------------
-
-		if ((x >= 255) && (x < 768))
-			G = 255;
-		if (x > 1023)
-			G = 0;
-
-		if ((x >= 1279) || (x < 255))
-			R = 255;
-		if ((x > 510) && (x <= 1024))
-			R = 0;
-
-		if ((x >= 767) && (x < 1280))
-			B = 255;
-		if (x <= 511)
-			B = 0;
-
-		//-----------------------------
-
-		if ((x >= 0) && (x <= 255))
-		{
-			G = x;
-		}
-		//-------------------
-
-		if ((x >= 256) && (x <= 511))
-		{
-			R = 255 - (x - 256);
-		}
-
-		//-------------------
-		if ((x >= 512) && (x <= 767))
-		{
-			B = x - 512;
-		}
-
-		//-------------------
-		if ((x >= 768) && (x <= 1023))
-		{
-			G = 255 - (x - 768);
-		}
-
-		//-------------------
-
-		if ((x >= 1024) && (x <= 1279))
-		{
-			R = x - 1024;
-		}
-
-		//-------------------
-		if ((x >= 1280) && (x <= 1535))
-		{
-			B = 255 - (x - 1280);
-		}
-
-		std::stringstream ssR;
-		ssR << std::setfill('0') << std::setw(sizeof(int) - 2) << std::hex << R;
-		std::string resR(ssR.str());
-
-		std::stringstream ssG;
-		ssG << std::setfill('0') << std::setw(sizeof(int) - 2) << std::hex << G;
-		std::string resG(ssG.str());
-
-		std::stringstream ssB;
-		ssB << std::setfill('0') << std::setw(sizeof(int) - 2) << std::hex << B;
-		std::string resB(ssB.str());
-
-		pinHexIn = resR + resG + resB;
-
-	}
-
 	void calcDimensions(Point& center, float& radius, float& thickness)
 	{
 		auto r = getRect();
 
 		center = Point((r.left + r.right) * 0.5f, (r.top + r.bottom) * 0.5f);
 		radius = (std::min)(r.getWidth(), r.getHeight()) * 0.4f;
-		thickness = radius * 0.2f;
+		thickness = radius * 0.1f;
 	}
 
 	int32_t MP_STDCALL hitTest(MP1_POINT point) override
@@ -223,7 +131,7 @@ public:
 
 		Point v(point.x - center.x, point.y - center.y);
 		float distSquared = v.x * v.x + v.y * v.y;
-		float outerSquared = (radius + thickness * 0.5f);
+		float outerSquared = (radius + thickness * 0.1f);
 		outerSquared *= outerSquared;
 
 		return distSquared <= outerSquared ? MP_OK : MP_FAIL;
@@ -241,8 +149,8 @@ public:
 		auto brushForeground = g.CreateSolidColorBrush(Color::White);
 		auto brushBackground = g.CreateSolidColorBrush(Color::FromHexString(pinHexIn));
 
-		const float startAngle = 35.0f; // angle between "straight-down" and start of arc. In degrees.
-		const float startAngleRadians = startAngle * (M_PI) / 180.f; // angle between "straight-down" and start of arc. In degrees.
+		const float startAngle = 30.0f; // angle between "straight-down" and start of arc. In degrees.
+		const float startAngleRadians = (startAngle * (M_PI) / 180.f); // angle between "straight-down" and start of arc. In degrees.
 		const float quarterTurnClockwise = (M_PI) * 0.5f;
 
 		StrokeStyleProperties strokeStyleProperties;
@@ -277,5 +185,5 @@ public:
 
 namespace
 {
-	auto r = Register<WirelessPatchMemGui>::withId(L"WirelessPatchMem");
+	auto r = Register<PatchMemKnobGui>::withId(L"PatchMemKnob");
 }
