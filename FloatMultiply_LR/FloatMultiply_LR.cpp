5a1fcc020b85f10d1d019a5b9cffaadc4e361ca4
maxbreges <mbreges@gmail.com>
2024-08-23 15:06:56 +0300

    FloatMultiply_LR

---
+++ FloatMultiply_LR/FloatMultiply_LR.cpp
@@ -0,0 +1,39 @@
+#include "mp_sdk_gui2.h"
+
+using namespace gmpi;
+
+class FloatMultiplyGui final : public SeGuiInvisibleBase
+{
+	void onSetValueIn()
+	{
+		pinValueOut = pinValueIn.getValue() * pinMultiplyby.getValue();
+	}
+
+ 	void onSetValueOut()
+	{
+		// pinValueOut changed
+	}
+
+ 	void onSetMultiplyby()
+	{
+		pinValueOut = pinValueIn.getValue() * pinMultiplyby.getValue();
+	}
+	 
+	FloatGuiPin pinValueIn; 	
+ 	FloatGuiPin pinMultiplyby;
+	FloatGuiPin pinValueOut;	
+
+public:
+	FloatMultiplyGui()
+	{
+		initializePin(pinValueIn, static_cast<MpGuiBaseMemberPtr2>(&FloatMultiplyGui::onSetValueIn));
+		initializePin( pinMultiplyby, static_cast<MpGuiBaseMemberPtr2>(&FloatMultiplyGui::onSetMultiplyby));
+		initializePin(pinValueOut, static_cast<MpGuiBaseMemberPtr2>(&FloatMultiplyGui::onSetValueOut));
+	}
+
+};
+
+namespace
+{
+	auto r = Register<FloatMultiplyGui>::withId(L"Float Multiply");
+}
