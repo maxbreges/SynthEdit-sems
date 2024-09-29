9c0f6f2536e92b3e2ee6e7cc260c4cf40c6ca984
maxbreges <mbreges@gmail.com>
2024-09-29 19:56:33 +0300

    String Length

---
+++ StringLength/StringLength.cpp
@@ -0,0 +1,65 @@
+#include "mp_sdk_gui2.h"
+
+using namespace std;
+using namespace gmpi;
+
+class StringLength final : public SeGuiInvisibleBase
+{
+	void onString()
+	{
+		std::string str = { String };
+		int count = str.size();
+		pinLengthBothOuts = count;
+		onSetBothOuts();
+	}
+
+ 	void onSetString()
+	{
+		String = pinString;
+		onString();
+	}
+
+	void onSetStringIn()
+	{
+		String = pinStringIn;
+		onString();
+	}
+	void onSetLength()
+	{		
+	}
+
+	void onSetLengthLeftOut()
+	{		
+	}
+
+	void onSetBothOuts()
+	{
+		pinLength = pinLengthBothOuts;
+		pinLengthLeftOut = pinLengthBothOuts;
+	}
+
+	StringGuiPin String;
+ 	StringGuiPin pinString;
+	StringGuiPin pinStringIn;
+ 	IntGuiPin pinLength;
+	IntGuiPin pinLengthLeftOut;
+	IntGuiPin pinLengthBothOuts;
+
+public:
+
+	StringLength()
+	{
+		initializePin(String, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onString));
+		initializePin( pinString, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onSetString) );
+		initializePin(pinStringIn, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onSetStringIn));
+		initializePin( pinLength, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onSetBothOuts) );
+		initializePin(pinLengthLeftOut, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onSetBothOuts));
+		initializePin(pinLengthBothOuts, static_cast<MpGuiBaseMemberPtr2>(&StringLength::onSetBothOuts));
+
+	}		
+};
+
+namespace
+{
+	auto r = Register<StringLength>::withId(L"StringLength");
+}
