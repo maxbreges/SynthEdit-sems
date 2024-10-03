57300bd77aeb78c35fdbaf35fb94e0fb9bcbb536
maxbreges <mbreges@gmail.com>
2024-10-03 22:19:46 +0300

    &gt; Gui Switches

---
+++ GuiSwitches/GuiSwitches.cpp
@@ -0,0 +1,16 @@
+#include "GuiSwitchText.h"
+#include <string>
+
+using namespace gmpi;
+
+typedef GuiSwitch<bool> GuiSwitchBool;
+typedef GuiSwitch<float> GuiSwitchFloat;
+typedef GuiSwitch<MpBlob> GuiSwitchBlob;
+typedef GuiSwitch<int32_t> GuiSwitchInt;
+typedef GuiSwitch<std::wstring> GuiSwitchText;
+
+GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, GuiSwitchBool, L"GuiSwitch > (Bool)");
+GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, GuiSwitchInt, L"GuiSwitch > (Float)");
+GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, GuiSwitchBlob, L"GuiSwitch > (Blob)");
+GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, GuiSwitchInt, L"GuiSwitch > (Int)");
+GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, GuiSwitchText, L"GuiSwitch > (Text)");
