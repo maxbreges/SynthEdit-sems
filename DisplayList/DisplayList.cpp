e168a7968d76dbeede79e2e6f8d66d8f2a40e34b
Max <mbreges@gmail.com>
2024-11-26 02:22:44 +0300

    Update DisplayList.cpp

--- DisplayList/DisplayList.cpp
+++ DisplayList/DisplayList.cpp
@@ -2,7 +2,7 @@
 #include "Drawing.h"
 #include "..\shared\it_enum_list.h"
 #include "..\shared\unicode_conversion.h"
-#include "C:\SynthEditSdk\SubControlsXp\TextSubcontrol.h"
+//#include "C:\SynthEditSdk\SubControlsXp\TextSubcontrol.h"
 #include "..\se_sdk3\mp_gui.h"
 #include <sstream>
 #include <iomanip>
