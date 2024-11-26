c4f131208314337b9c6593506c9cc68205e1a280
Max <mbreges@gmail.com>
2024-11-26 12:31:45 +0300

    Update DisplayList.cpp

--- DisplayList/DisplayList.cpp
+++ DisplayList/DisplayList.cpp
@@ -2,7 +2,7 @@
 #include "Drawing.h"
 #include "..\shared\it_enum_list.h"
 #include "..\shared\unicode_conversion.h"
-#include ".\SubControlsXp\TextSubcontrol.h"
+#include "..\shared\TextSubcontrol.h"
 #include "..\se_sdk3\mp_gui.h"
 #include <sstream>
 #include <iomanip>
