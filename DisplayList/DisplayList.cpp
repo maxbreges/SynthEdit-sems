dee29a5ee9e665a1605671e0c8116765b2ed3426
maxbreges <mbreges@gmail.com>
2024-11-26 18:43:53 +0300

    Display List

--- DisplayList/DisplayList.cpp
+++ DisplayList/DisplayList.cpp
@@ -1,7 +1,7 @@
 #include "mp_sdk_gui2.h"
 #include "Drawing.h"
-//#include "..\shared\unicode_conversion.h"
-//#include ".\SubControlsXp\TextSubcontrol.h"
+#include "..\shared\unicode_conversion.h"
+#include ".\SubControlsXp\TextSubcontrol.h"
 #include "..\se_sdk3\mp_gui.h"
 #include <sstream>
 #include <iomanip>
@@ -9,11 +9,12 @@
 //#include <string>
 //#include <iostream>
 
+
 using namespace std;
 using namespace gmpi;
 using namespace gmpi_gui;
 using namespace GmpiDrawing;
-//using namespace JmUnicodeConversions;
+using namespace JmUnicodeConversions;
 
 GmpiDrawing_API::MP1_POINT pointPrevious;
 GmpiGui::PopupMenu nativeMenu;
