12615e928bdd26b2699ddcec543b08bd7b1e5785
Max <mbreges@gmail.com>
2024-11-26 12:05:41 +0300

    Display List

--- DisplayList/DisplayList.cpp
+++ DisplayList/DisplayList.cpp
@@ -2,13 +2,10 @@
 #include "Drawing.h"
 #include "..\shared\it_enum_list.h"
 #include "..\shared\unicode_conversion.h"
-//#include "C:\SynthEditSdk\SubControlsXp\TextSubcontrol.h"
 #include "..\se_sdk3\mp_gui.h"
 #include <sstream>
 #include <iomanip>
-//#include <algorithm>
-//#include <string>
-//#include <iostream>
+
 
 using namespace std;
 using namespace gmpi;
