6043f3b3a1b93a9bb4ff1909f01d78414eec2a69
Max <mbreges@gmail.com>
2024-12-03 18:58:54 +0300

    MIDI Learn2

---
+++ MIDILearn2/MIDILearn2.cpp
@@ -0,0 +1,105 @@
+#include "mp_sdk_audio.h"
+#include "mp_midi.h"
+#include <array>
+//#include <climits>
+
+SE_DECLARE_INIT_STATIC_FILE(mxMIDILearn2);
+
+#define NOTE_ON     0x90
+#define NOTE_OFF    0x80
+
+using namespace gmpi;
+using namespace GmpiMidi;
+using namespace midi_2_0;
+
+class MIDILearn final : public MpBase2
+{
+	MidiInPin pinMIDIIn;
+	EnumOutPin pinNote;
+	BoolInPin pinGate;
+	BoolOutPin pinGateOut;
+	IntOutPin pinIntOut;
+	IntOutPin pinIntOutToGui;
+
+	gmpi::midi_2_0::MidiConverter2 midiConverter;
+
+	int messageSize = 0;
+	
+
+public:
+	MIDILearn() :
+		// init the midi converter
+		midiConverter(
+			// provide a lambda to accept converted MIDI 2.0 messages
+			[this](const midi::message_view& msg, int offset)
+			{
+				onMidi2Message(msg);
+			}
+		)
+	{
+		initializePin( pinMIDIIn );
+		initializePin( pinNote );
+		initializePin(pinGate);
+		initializePin(pinGateOut);
+		initializePin(pinIntOut);
+		initializePin(pinIntOutToGui);
+	}
+
+	int32_t open() override
+	{
+		MpBase2::open();    // always call the base class
+		return gmpi::MP_OK;
+	}
+
+	// passes all MIDI to the converter.
+	void onMidiMessage(int pin, unsigned char* midiMessage, int size) override
+	{
+		midi::message_view msg((const uint8_t*)midiMessage, size);
+
+		// convert everything to MIDI 2.0
+		midiConverter.processMidi(msg, -1);
+	}
+
+	// put your midi handling code in here.
+	void onMidi2Message(const midi::message_view& msg)
+	{
+		const auto header = gmpi::midi_2_0::decodeHeader(msg);
+
+		messageSize = (int)(size_t)msg.size();
+		// only 8-byte messages supported. only 16 channels supported
+		if (header.messageType != gmpi::midi_2_0::ChannelVoice64)
+			return;
+
+		if (pinGate)
+		{
+			
+
+			switch (header.status)
+			{
+			case gmpi::midi_2_0::NoteOn:
+			{
+				const auto note = gmpi::midi_2_0::decodeNote(msg);
+				pinNote = (int)(uint8_t)note.noteNumber;
+				pinGateOut = false;
+				pinIntOut = pinNote;
+				pinIntOutToGui = pinNote;
+			}
+			break;
+
+			case gmpi::midi_2_0::NoteOff:
+			{
+				const auto note = gmpi::midi_2_0::decodeNote(msg);
+				pinNote = (int)(uint8_t)note.noteNumber;
+					pinGateOut = true;
+			}
+			break;
+			}
+
+		};
+	}
+};
+
+namespace
+{
+	auto r = Register<MIDILearn>::withId(L"MIDI Learn2");
+}
---
+++ MIDILearn2/MIDILearn2Gui.cpp
@@ -0,0 +1,146 @@
+﻿#include "mp_sdk_gui2.h"
+#include <sstream>
+
+using namespace gmpi;
+
+class MIDILearnGui final : public SeGuiInvisibleBase
+{ 	
+
+ 	void onSetAnimationPosition()
+	{
+			pinGate = pinAnimationPosition;			
+	}
+
+	void onSetGate()
+	{ 
+
+	}
+
+	void onSetGateIn()
+	{
+		if (pinGateIn)
+			pinAnimationPosition = false;
+
+		onSetAnimationPosition();
+	}
+
+	std::string acc;
+
+	void onSetNoteIn()
+	{
+		pinNoteOut = pinNoteIn;
+
+		if (!pinSwitch)
+		{
+			wchar_t accsh[] = { L'♯', L'\0' };
+			pinAccidentals = accsh;;
+			Note2CharSharps();
+		}
+
+		if (pinSwitch)
+		{
+			wchar_t accfl[] = { L'♭', L'\0' };
+			pinAccidentals = accfl;
+			Note2CharFlats();
+		}
+	}	
+	
+	void onSetSwitch()
+	{
+
+	}
+ 	
+ 	FloatGuiPin pinAnimationPosition;
+	BoolGuiPin pinSwitch;
+	BoolGuiPin pinGate;
+	BoolGuiPin pinGateIn;
+	StringGuiPin pinNote;
+	IntGuiPin pinNoteIn;	
+	StringGuiPin pinAccidentals; //hidden pin, memory cell of a string
+	IntGuiPin pinNoteOut;
+
+public:
+	MIDILearnGui()
+	{
+		initializePin( pinAnimationPosition, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetAnimationPosition) );
+		initializePin(pinSwitch, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetSwitch));
+		initializePin(pinGate, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetAnimationPosition));
+		initializePin(pinGateIn, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetGateIn));
+		initializePin(pinNote, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetAnimationPosition));
+		initializePin(pinNoteIn, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetNoteIn));
+		initializePin(pinAccidentals, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetSwitch));
+		initializePin(pinNoteOut, static_cast<MpGuiBaseMemberPtr2>(&MIDILearnGui::onSetNoteIn));
+	}
+
+	int32_t Note2CharSharps()
+	{
+		int mi = pinNoteIn;
+		signed int oct_v = mi / 12 - 1;
+
+		std::string nt3;
+		acc = pinAccidentals;
+		// with switch
+		switch (mi % 12)
+		{
+		case 0: nt3 = "C"; break;
+		case 1: nt3 = "C" + acc; break;
+		case 2: nt3 = "D"; break;
+		case 3: nt3 = "D" + acc; break;
+		case 4: nt3 = "E"; break;
+		case 5: nt3 = "F"; break;
+		case 6: nt3 = "F" + acc; break;
+		case 7: nt3 = "G"; break;
+		case 8: nt3 = "G" + acc; break;
+		case 9: nt3 = "A"; break;
+		case 10: nt3 = "A" + acc; break;
+		case 11: nt3 = "B"; break;
+		}
+
+		std::stringstream oct;
+		oct << std::fixed << oct_v;
+		std::string oct_(oct.str());
+
+		pinNote = nt3 + oct_;
+
+		return 0;
+	}
+
+	int32_t Note2CharFlats()
+	{
+		int mi = pinNoteIn;
+		signed int oct_v = mi / 12 - 1;
+
+		std::string nt3;
+		acc = pinAccidentals;
+		// with switch
+		switch (mi % 12)
+		{
+		case 0: nt3 = "C"; break;
+		case 1: nt3 = "D" + acc; break;
+		case 2: nt3 = "D"; break;
+		case 3: nt3 = "E" + acc; break;
+		case 4: nt3 = "E"; break;
+		case 5: nt3 = "F"; break;
+		case 6: nt3 = "G" + acc; break;
+		case 7: nt3 = "G"; break;
+		case 8: nt3 = "A" + acc; break;
+		case 9: nt3 = "A"; break;
+		case 10: nt3 = "B" + acc; break;
+		case 11: nt3 = "B"; break;
+		}
+
+		std::stringstream oct;
+		oct << std::fixed << oct_v;
+		std::string oct_(oct.str());
+
+		pinNote = nt3 + oct_;
+
+		return 0;
+	}
+
+};
+
+namespace
+{
+	auto r = Register<MIDILearnGui>::withId(L"MIDI Learn2");
+}
