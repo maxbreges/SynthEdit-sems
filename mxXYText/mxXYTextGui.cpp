feec3a000ab385d24bd60a9ff14ea63eb954f939
Max <mbreges@gmail.com>
2024-12-17 04:12:03 +0300

    Update mxXYTextGui.cpp

--- mxXYText/mxXYTextGui.cpp
+++ mxXYText/mxXYTextGui.cpp
@@ -1,15 +1,11 @@
 #include "mp_sdk_gui2.h"
-#include "../shared/xplatform_modifier_keys.h"
-//#include "../shared/it_enum_list.h"
-#include "../shared/unicode_conversion.h"
+#include "Drawing.h"
 #include <cmath>
 #include <string>
 
 using namespace std;
 using namespace gmpi;
-using namespace gmpi_gui;
 using namespace GmpiDrawing;
-using namespace JmUnicodeConversions;
 
 class mxXYTextGui final : public gmpi_gui::MpGuiGfxBase
 {
@@ -130,66 +126,46 @@ public:
         initializePin(pinMouseDown, static_cast<MpGuiBaseMemberPtr2>(&mxXYTextGui::onSetMouseDown));
 	}
 
-	int32_t MP_STDCALL OnRender(GmpiDrawing_API::IMpDeviceContext* drawingContext ) override
+    int32_t MP_STDCALL OnRender(GmpiDrawing_API::IMpDeviceContext* drawingContext) override
     {
         Graphics g(drawingContext);
-        ClipDrawingToBounds z(g, getRect());
         Rect r = getRect();
-
-        // Get control dimensions
         float lineSize = pinLineSize.getValue();
-        float halfLineSize = lineSize * 0.5f;
         float textboxwidth = pinWidth.getValue() - lineSize;
         float textboxheight = pinHeight.getValue() - lineSize;
-        float tboxwidthhlf = textboxwidth * .5f;
-        float tboxheighthlf = textboxheight * .5f;
-        // Ensure normalized positions
+        float halfLineSize = lineSize * 0.5f;
         float normalisedX = (max)(0.0f, (min)(1.0f, pinPositionX.getValue()));
         float normalisedY = 1.0f - (max)(0.0f, (min)(1.0f, pinPositionY.getValue()));
-        // Adjust for padding and border size
         float adjustedWidth = r.getWidth() - (textboxwidth + halfLineSize * 2.0f);
         float adjustedHeight = r.getHeight() - (textboxheight + halfLineSize * 2.0f);
+
         float x = halfLineSize + normalisedX * adjustedWidth;
         float y = halfLineSize + normalisedY * adjustedHeight;
-        // Rectangle for the text box
-        GmpiDrawing::Rect dest_rect(x, y, x + textboxwidth, y + textboxheight);
-        // Rectangle for text (with vertical adjustment if needed)
+
         GmpiDrawing::Rect text_rect(x, pinFontYAdjust + y, x + textboxwidth, pinFontYAdjust + y + textboxheight);
-        // Draw rectangle with correct line size
+
         auto brushrectline = g.CreateSolidColorBrush(Color::FromHexString(pinLineARGB));
         auto brushshape = g.CreateSolidColorBrush(Color::FromHexString(pinShapeARGB));
+        float tboxwidthhlf = textboxwidth * .5f;
+        float tboxheighthlf = textboxheight * .5f;
 
-        if (!pinShape)
         {
             Point center = { x + tboxwidthhlf,y + tboxheighthlf };
             GmpiDrawing::Ellipse circle(center, tboxwidthhlf, tboxheighthlf);
             g.FillEllipse(circle, brushshape);
             g.DrawEllipse(circle, brushrectline, pinLineSize);
         }
-        else
-        {
-
-            g.FillRectangle(dest_rect, brushshape);
-            g.DrawRectangle(dest_rect, brushrectline, pinLineSize);
-        }
-
-        // Start Font Draw: setting QUICK AND DIRTY!!! YOU CAN USE STYLES FROM META DATA!!
-        std::string str = pinFont;
-        int font_size_ = pinFontSize.getValue();
-        GmpiDrawing_API::MP1_FONT_WEIGHT fontWeight = GmpiDrawing_API::MP1_FONT_WEIGHT_HEAVY;
+        float font_size = pinFontSize;
+        std::string str = { pinFont };
         const char* fontFace = str.c_str();
-        float dipFontSize = (max(font_size_, 1.f));
-        TextFormat textFormat = g.GetFactory().CreateTextFormat(dipFontSize, fontFace, fontWeight);
+        GmpiDrawing_API::MP1_FONT_WEIGHT fontWeight = GmpiDrawing_API::MP1_FONT_WEIGHT_HEAVY;
+        auto textFormat = GetGraphicsFactory().CreateTextFormat(font_size, fontFace, fontWeight);
         textFormat.SetTextAlignment(TextAlignment::Center);
         textFormat.SetParagraphAlignment(ParagraphAlignment::Center);
-        textFormat.SetImprovedVerticalBaselineSnapping();
-        textFormat.SetWordWrapping(WordWrapping::Wrap);
         auto brushfont = g.CreateSolidColorBrush(Color::FromHexString(pinFontARGB));
         g.DrawTextW(pinText, textFormat, text_rect, brushfont, (int32_t)DrawTextOptions::Clip);
-        // End FONT Draw //
 
-
-        return MP_OK;
+        return gmpi::MP_OK;
     }
 };
 
