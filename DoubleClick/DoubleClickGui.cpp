314745600115fdfe20793ce89c8b9519212a9f3e
Max <mbreges@gmail.com>
2025-03-13 01:28:15 +0300

    dbl-clk fix

--- DoubleClick/DoubleClickGui.cpp
+++ DoubleClick/DoubleClickGui.cpp
@@ -10,6 +10,7 @@ class DoubleClickGui final : public SeGuiInvisibleBase
 private:
     int count = 0;
     std::mutex mtx;
+    bool processing = false;  // Flag to indicate if processing is ongoing
 
 public:
     void onSetMouseDown()
@@ -26,14 +27,14 @@ public:
             // Start countdown in a separate thread
             std::thread([this, countDown]()
                 {
-                    for (int i = countDown; i >= 0; --i)
-                    {
-                        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                  //  for (int i = countDown; i >= 0; --i)
+                   // {
+                        std::this_thread::sleep_for(std::chrono::milliseconds(countDown));
                         {
                             std::lock_guard<std::mutex> lock(mtx);
-                            pinIntOut = i;
+                           // pinIntOut = i;
                         }
-                    }
+                   // }
 
                     // Reset count when countdown is finished
                     {
@@ -41,6 +42,7 @@ public:
                         count = 0;
                         pinClickCount = count; // Reset pinClickCount
                     }
+                   
                 }
             ).detach();
         }
@@ -51,7 +53,17 @@ public:
             {
                 pinAnimPos = pinResetValue;
                 pinClickCount = 0; // Reset to avoid repeated actions
+
+                // Check if we are already processing
+                if (processing)
+                {
+                    return; // Ignore further clicks while processing
+                }
+
+                processing = true; // Set flag to indicate processing has started
             }
+            // Reset the processing flag to allow future clicks
+          processing = false;
         }
     }
 
