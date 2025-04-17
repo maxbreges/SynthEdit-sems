41146d1aa8b157cfd1cfe675358c56baf935b8b8
Max <mbreges@gmail.com>
2025-04-17 22:01:53 +0300

    Update IntEntryGui.h

--- IntEntry/IntEntryGui.h
+++ IntEntry/IntEntryGui.h
@@ -1,7 +1,7 @@
 #ifndef INTENTRYGUI_H_INCLUDED
 #define INTENTRYGUI_H_INCLUDED
 
-#include "../SubControlsXp/TextSubcontrol.h"
+#include "TextSubcontrol.h"
 #include "FontCache.h"
 
 class IntEntryGui : public TextSubcontrol
