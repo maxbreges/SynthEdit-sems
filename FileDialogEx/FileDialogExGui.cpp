5d08b58b865fcf6d9c88c5f4d394d687abc54acc
Max <mbreges@gmail.com>
2025-07-31 20:10:28 +0300

    Update FileDialogExGui.cpp

--- FileDialogEx/FileDialogExGui.cpp
+++ FileDialogEx/FileDialogExGui.cpp
@@ -1,5 +1,4 @@
 #include "./FileDialogExGui.h"
-
 #include "../shared/unicode_conversion.h"
 #include "../shared/it_enum_list.h"
 #include "../shared/string_utilities.h"
@@ -18,7 +17,7 @@ GMPI_REGISTER_GUI(MP_SUB_TYPE_GUI2, FileDialogExGui, L"FileDialogEx");
 
 // Helper function for case-insensitive string comparison.
 // You might already have a similar utility in string_utilities.h
-bool iequals(const std::string & a, const std::string & b)
+bool iequals(const std::string& a, const std::string& b)
 {
 	return std::equal(a.begin(), a.end(),
 		b.begin(), b.end(),
@@ -42,23 +41,16 @@ FileDialogExGui::FileDialogExGui() :
 	initializePin(pinOpened);
 }
 
-std::string fileext;
-
 void FileDialogExGui::onSetItemsList()
 {
 	std::wstring itemsList = pinParentPath.getValue();
 	m_fileNames.clear();
-
 	std::wistringstream wss(itemsList);
 	std::wstring item;
-
 	while (std::getline(wss, item, L',')) {
 		if (!item.empty()) // Avoid empty entries
 			m_fileNames.push_back(item);
 	}
-
-	//pinDebug = L"onSetItemsList()";
-	//onSetChoice();
 }
 
 void FileDialogExGui::onSetChoice()
@@ -152,20 +144,18 @@ void FileDialogExGui::onSetSelectedFile()
 {
 	if (m_fileNames.empty())
 	{
-		pinChoice = -1; // Or another default value to indicate no selection
+		pinChoice = -1;
 		return;
 	}
 
-	std::wstring selectedFile = fs::path(pinFileName).stem().wstring();
-
+	std::wstring selectedFile = fs::path(pinFileName.getValue()).stem().wstring();
 	auto it = std::find(m_fileNames.begin(), m_fileNames.end(), selectedFile);
-
 	if (it != m_fileNames.end())
 	{
 		pinChoice = static_cast<int>(std::distance(m_fileNames.begin(), it));
 	}
 	else
 	{
-		pinChoice = -1; // File not found, set to -1 (or appropriate value)
+		pinChoice = -1;
 	}
 }
