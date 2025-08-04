8cdb80e830db1372b10633f5760467ecf05e012d
Max <mbreges@gmail.com>
2025-08-04 18:41:28 +0300

    FDx

--- FileDialog/FileDialogGui.cpp
+++ FileDialog/FileDialogGui.cpp
@@ -29,61 +29,6 @@ FileDialogGui::FileDialogGui() :
 	initializePin(pinOpened);
 }
 
-
-std::string FileDialogGui::getDefaultFolder(std::wstring extension)
-{
-	const std::wstring searchFilename = L"dummy." + extension;
-	const auto fullFileName = uiHost.resolveFilename(searchFilename.c_str());
-	return JmUnicodeConversions::WStringToUtf8(fullFileName.substr(0, fullFileName.find(L"dummy") - 1));
-}
-
-void FileDialogGui::onSetTrigger()
-{
-	pinOpened = pinTrigger;
-	// trigger on mouse-up
-	if (!pinTrigger  && m_prev_trigger == true) // dialog triggered on mouse-up (else dialog grabs focus, button never resets)
-	{
-		std::ostringstream oss;
-		oss << "onSetTrigger called: pinTrigger=" << pinTrigger << ", m_prev_trigger=" << m_prev_trigger << "\n";
-		pinDebug = oss.str();
-		std::wstring filename = pinFileName;
-		std::wstring file_extension = pinFileExtension;
-
-		IMpGraphicsHostBase* dialogHost = 0;
-		getHost()->queryInterface(SE_IID_GRAPHICS_HOST_BASE, reinterpret_cast<void**>(&dialogHost));
-
-		if (dialogHost != 0)
-		{
-			//pinOpened = true;
-			int dialogMode = (int)pinSaveMode;
-			dialogHost->createFileDialog(dialogMode, nativeFileDialog.GetAddressOf());
-
-			if ( ! nativeFileDialog.isNull())
-			{
-				nativeFileDialog.AddExtensionList(pinFileExtension);
-
-				auto filename = pinFileName.getValue();
-				if (!filename.empty())
-				{
-					filename = uiHost.resolveFilename(filename);
-					nativeFileDialog.SetInitialFullPath(JmUnicodeConversions::WStringToUtf8(filename));
-				}
-				else
-				{
-					nativeFileDialog.setInitialDirectory(getDefaultFolder(pinFileExtension));
-				}
-
-				nativeFileDialog.ShowAsync([this](int32_t result) -> void { this->OnFileDialogComplete(result); });
-			}
-		}
-	}
-
-	m_prev_trigger = pinTrigger;
-	//pinOpened = m_prev_trigger;
-}
-
-std::string fileext;
-
 void FileDialogGui::onSetItemsList()
 {
 	std::wstring itemsList = pinItemsListIn.getValue();
@@ -97,7 +42,7 @@ void FileDialogGui::onSetItemsList()
 			m_fileNames.push_back(item);
 	}
 
-	//pinDebug = L"onSetItemsList()";
+	pinDebug = L"onSetItemsList()";
 	//onSetChoice();
 }
 
@@ -129,9 +74,58 @@ void FileDialogGui::onSetChoice()
 	pinDebug = debugOutput;
 }
 
+std::string FileDialogGui::getDefaultFolder(std::wstring extension)
+{
+	const std::wstring searchFilename = L"dummy." + extension;
+	const auto fullFileName = uiHost.resolveFilename(searchFilename.c_str());
+	return JmUnicodeConversions::WStringToUtf8(fullFileName.substr(0, fullFileName.find(L"dummy") - 1));
+}
+
+void FileDialogGui::onSetTrigger()
+{
+	// trigger on mouse-up
+	if (pinTrigger == false && m_prev_trigger == true) // dialog triggered on mouse-up (else dialog grabs focus, button never resets)
+	{
+		std::wstring filename = pinFileName;
+		std::wstring file_extension = pinFileExtension;
+
+		IMpGraphicsHostBase* dialogHost = 0;
+		getHost()->queryInterface(SE_IID_GRAPHICS_HOST_BASE, reinterpret_cast<void**>(&dialogHost));
+
+		if (dialogHost != 0)
+		{
+			pinOpened = true;
+			int dialogMode = (int)pinSaveMode;
+			dialogHost->createFileDialog(dialogMode, nativeFileDialog.GetAddressOf());
+
+			if (!nativeFileDialog.isNull())
+			{
+				nativeFileDialog.AddExtensionList(pinFileExtension);
+
+				auto filename = pinFileName.getValue();
+				if (!filename.empty())
+				{
+					filename = uiHost.resolveFilename(filename);
+					nativeFileDialog.SetInitialFullPath(JmUnicodeConversions::WStringToUtf8(filename));
+				}
+				else
+				{
+					nativeFileDialog.setInitialDirectory(getDefaultFolder(pinFileExtension));
+				}
+
+				nativeFileDialog.ShowAsync([this](int32_t result) -> void { this->OnFileDialogComplete(result); });
+			}
+		}
+	}
+
+	m_prev_trigger = pinTrigger;
+}
+
+std::string fileext;
+
 void FileDialogGui::OnFileDialogComplete(int32_t result)
 {
-	//pinOpened = false;
+	pinOpened = false;
 
 	if (result != gmpi::MP_OK)
 	{
@@ -184,32 +178,37 @@ void FileDialogGui::OnFileDialogComplete(int32_t result)
 		}
 
 		pinFileName = filepath;
-		pinDebug = filepath;
+		//pinDebug = filepath;
+	
 	}
 
-	auto parentPath = fs::path(pinFileName).parent_path();
+	std::string filepath = pinFileName;
+	auto parentPath = std::experimental::filesystem::path(filepath).parent_path();
 	pinDirectory = parentPath;
-	updateItemsList(parentPath.string());	
+	updateItemsList(parentPath.string());
 	nativeFileDialog.setNull(); // release it.
 }
 
 void FileDialogGui::updateItemsList(const fs::path& directory)
 {
-	m_fileNames.clear(); // Clear previous file names
+	m_fileNames.clear();	
 
 	if (fs::exists(directory) && fs::is_directory(directory))
 	{
+		pinDebug = L"updateItemList";
 		for (const auto& entry : fs::directory_iterator(directory))
 		{
-			if (entry.is_regular_file())
+			
+			if (std::experimental::filesystem::is_regular_file(entry.path()))
 			{
-				auto file_extension = entry.path().extension().string().substr(1);
+				auto ext = entry.path().extension().string();
+				if (!ext.empty() && ext[0] == '.') {
+					ext = ext.substr(1);
+				}
 
-				// Now check if the extension matches the requested one
-				if (file_extension == fileext) // no operator matches these operands 
+				if (ext == fileext)
 				{
-					// Store the full filename (with extension) in m_fileNames
-					m_fileNames.push_back(entry.path().stem().wstring()); // Store only the file name without extension
+					m_fileNames.push_back(entry.path().stem().wstring());
 				}
 			}
 		}
@@ -237,8 +236,8 @@ void FileDialogGui::onSetSelectedFile()
 		pinChoice = -1; // Or another default value to indicate no selection
 		return;
 	}
-
-	std::wstring selectedFile = fs::path(pinFileName).stem().wstring();
+	std::string filepath = pinFileName;
+	std::wstring selectedFile = std::experimental::filesystem::path(filepath).stem().wstring();
 
 	auto it = std::find(m_fileNames.begin(), m_fileNames.end(), selectedFile);
 
