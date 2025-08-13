80087ae5ebdce95584291f996bfc0ca3cecac7e9
Max <mbreges@gmail.com>
2025-08-13 21:41:43 +0300

    Update FileDialogGui.h

--- FileDialog/FileDialogGui.h
+++ FileDialog/FileDialogGui.h
@@ -6,21 +6,46 @@
 #include <vector>
 #include <string>
 #include <filesystem>
+#include <set>
 
 namespace fs = std::filesystem;
+
+struct ci_wstring_compare
+{
+	bool operator()(const std::wstring& a, const std::wstring& b) const
+	{
+		auto itA = a.begin();
+		auto itB = b.begin();
+
+		while (itA != a.end() && itB != b.end())
+		{
+			wchar_t chA = towlower(*itA);
+			wchar_t chB = towlower(*itB);
+			if (chA < chB)
+				return true;
+			else if (chA > chB)
+				return false;
+			++itA;
+			++itB;
+		}
+		// If all characters so far are equal, the shorter string is "less"
+		return a.size() < b.size();
+	}
+};
 
 class FileDialogGui : public SeGuiInvisibleBase
 {
 	bool m_prev_trigger;
 	GmpiGui::FileDialog nativeFileDialog;
 	std::vector<std::wstring> m_fileNames;
+	std::set<std::wstring, ci_wstring_compare> m_fileNamesSet;
 
 public:
 	FileDialogGui();
 
 	// overrides.
 
-private:
+//private:
 	void onSetTrigger();
 	void OnFileDialogComplete(int32_t result);
 	std::string getDefaultFolder(std::wstring extension);
