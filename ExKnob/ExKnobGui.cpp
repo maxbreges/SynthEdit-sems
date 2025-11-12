e68c7d9e87c4fd69a1410066a285c93b4171d9f8
Max <mbreges@gmail.com>
2025-11-12 12:45:04 +0300

    ex knob update

--- ExKnob/ExKnobGui.cpp
+++ ExKnob/ExKnobGui.cpp
@@ -4,6 +4,8 @@
 #include <math.h>
 #include "../sharedLegacyWidgets/SubControlBase.h"
 #include "../shared/xplatform_modifier_keys.h"
+#include <iomanip>
+#include <sstream>
 
 using namespace gmpi;
 using namespace gmpi_gui;
@@ -11,10 +13,38 @@ using namespace GmpiDrawing;
 
 GmpiDrawing_API::MP1_POINT pointPrevious;
 
+// Constants for better readability
+const float WHEEL_SCROLL_FACTOR = 1.0f / 12000.0f;
+const float CIRCLE_SIZE_MULTIPLIER = 0.4f;
+const float THICKNESS_MULTIPLIER = 0.2f;
+const int HEX_WIDTH = 2; // Width for hex color string
+
 class ExKnobGui final : public gmpi_gui::MpGuiGfxBase
 {
+	float clamp(float value, float minVal, float maxVal) //help clamp function
+	{
+		return (value < minVal) ? minVal : (value > maxVal) ? maxVal : value;
+	}
+
+	float circleSize;
+
 	void onSetAnimationPosition()
 	{
+
+		if (pinColorAdj)
+		{
+			AnimPosToHex();
+			pinHex = pinHexIn;
+			pinBackground = pinHexIn;
+			pinHexOut = pinHexIn;
+		}
+		else
+		{
+			pinHexIn = pinHex;
+			pinBackground = pinHex;
+			pinHexOut = pinHex;
+		}
+
 		// Invalidate for redraw
 		invalidateRect();
 
@@ -29,6 +59,7 @@ class ExKnobGui final : public gmpi_gui::MpGuiGfxBase
 		{
 			pinCurrentStep = current_step;
 		}
+		
 	}
 
  	void onSetHint()
@@ -76,6 +107,12 @@ class ExKnobGui final : public gmpi_gui::MpGuiGfxBase
 		//
 	}
 
+	void onSetCircleSize()
+	{		
+		circleSize = clamp(pinCircleSize, 0.0f, 1.0f);
+		invalidateRect();
+	}
+
  	FloatGuiPin pinAnimationPosition;
  	StringGuiPin pinHint;
  	StringGuiPin pinMenuItems;
@@ -86,6 +123,15 @@ class ExKnobGui final : public gmpi_gui::MpGuiGfxBase
  	BoolGuiPin pinReadOnly;
 	IntGuiPin pinSteps;
 	IntGuiPin pinCurrentStep;
+	IntGuiPin pinStyle;
+	StringGuiPin pinHexIn;
+	FloatGuiPin pinCircleSize;
+	FloatGuiPin pinLineThickness;
+	BoolGuiPin pinColorAdj;
+	StringGuiPin pinHex;
+	StringGuiPin pinHexOut;
+	FloatGuiPin pinStartAngle;
+	BoolGuiPin pinCtrlClick;
 
 public:
 	ExKnobGui()
@@ -100,6 +146,15 @@ public:
 		initializePin(pinReadOnly, static_cast<MpGuiBaseMemberPtr2>(&ExKnobGui::onSetRead_only) );
 		initializePin(pinSteps, static_cast<MpGuiBaseMemberPtr2>(&ExKnobGui::onSetSteps));
 		initializePin(pinCurrentStep, static_cast<MpGuiBaseMemberPtr2>(&ExKnobGui::onSetCurrentStep));
+		initializePin(pinStyle);
+		initializePin(pinHexIn);
+		initializePin(pinCircleSize, static_cast<MpGuiBaseMemberPtr2>(&ExKnobGui::onSetCircleSize));
+		initializePin(pinLineThickness);
+		initializePin(pinColorAdj);
+		initializePin(pinHex);
+		initializePin(pinHexOut);
+		initializePin(pinStartAngle);
+		initializePin(pinCtrlClick);
 	}
 
 	int32_t MP_STDCALL onPointerDown(int32_t flags, GmpiDrawing_API::MP1_POINT point) override
@@ -108,6 +163,11 @@ public:
 		if (pinReadOnly == true || (flags & GG_POINTER_FLAG_FIRSTBUTTON) == 0)
 		{
 			return gmpi::MP_OK; // Indicate successful hit, so right-click menu can show.
+		}
+
+		if (flags & gmpi_gui_api::GG_POINTER_KEY_CONTROL)
+		{
+			pinCtrlClick = true;
 		}
 
 		pointPrevious = point;	// note first point.
@@ -129,7 +189,8 @@ public:
 
 		releaseCapture();
 		pinMouseDown = false;
-
+		pinColorAdj = false;
+		pinCtrlClick = false;
 		return gmpi::MP_OK;
 	}
 
@@ -201,6 +262,17 @@ public:
 
 	int32_t MP_STDCALL onMouseWheel(int32_t flags, int32_t delta, MP1_POINT point) override
 	{
+
+		if (flags & gmpi_gui_api::GG_POINTER_KEY_CONTROL) // <cntr> key magnifies
+		{
+			float new_pos = pinAnimationPosition + delta * WHEEL_SCROLL_FACTOR; // Updated scrolling logic
+			new_pos = max(0.f, min(new_pos, 1.f)); // Clamp value between 0 and 1
+			pinAnimationPosition = new_pos;
+		}
+
+		else
+		{	
+
 		int32_t steps = pinSteps.getValue(); // total number of steps
 
 		float new_pos = pinAnimationPosition;
@@ -223,6 +295,9 @@ public:
 
 		pinAnimationPosition = snapped_pos;
 		pinCurrentStep = current_step;
+
+		}
+
 		return gmpi::MP_OK;
 	}
 
@@ -233,11 +308,23 @@ public:
 			return gmpi::MP_UNHANDLED;
 		}
 
+		if (flags & gmpi_gui_api::GG_POINTER_KEY_SHIFT)
+		{
+			pinColorAdj = true;
+		}
+		if ((flags & gmpi_gui_api::GG_POINTER_KEY_SHIFT) == 0)
+		{
+			pinColorAdj = false;
+		}
+
 		Point offset(point.x - pointPrevious.x, point.y - pointPrevious.y); // TODO overload subtraction.
 
 		float coarseness = 0.005f;
 
 		if (flags & gmpi_gui_api::GG_POINTER_KEY_CONTROL) // <cntr> key magnifies
+
+			pinCtrlClick = false;
+			
 			coarseness = 0.001f;
 
 		float new_pos = pinAnimationPosition;
@@ -250,15 +337,6 @@ public:
 		pinAnimationPosition = new_pos;
 
 		return gmpi::MP_OK;
-	}
-
-	void calcDimensions(Point& center, float& radius, float& thickness)
-	{
-		auto r = getRect();
-
-		center = Point((r.left + r.right) * 0.5f, (r.top + r.bottom) * 0.5f);
-		radius = (std::min)(r.getWidth(), r.getHeight()) * 0.4f;
-		thickness = radius * 0.2f;
 	}
 
 	int32_t MP_STDCALL hitTest(MP1_POINT point) override
@@ -276,47 +354,92 @@ public:
 		return distSquared <= outerSquared ? MP_OK : MP_FAIL;
 	}
 
-	int32_t MP_STDCALL OnRender(GmpiDrawing_API::IMpDeviceContext* drawingContext) override
-	{
-		Graphics g(drawingContext);
+	int32_t AnimPosToHex() {
+		int x = pinAnimationPosition * 1535; // Scale animation position to range
+		int R = 0;
+		int G = 0;
+		int B = 0;
 
+		// Determine RGB values based on x
+		if ((x >= 255) && (x < 768)) G = 255;
+		if (x > 1023) G = 0;
+		if ((x >= 1279) || (x < 255)) R = 255;
+		if ((x > 510) && (x <= 1024)) R = 0;
+		if ((x >= 767) && (x < 1280)) B = 255;
+		if (x <= 511) B = 0;
+
+		// Additional color transitions
+		if ((x >= 0) && (x <= 255)) G = x;
+		if ((x >= 256) && (x <= 511)) R = 255 - (x - 256);
+		if ((x >= 512) && (x <= 767)) B = x - 512;
+		if ((x >= 768) && (x <= 1023)) G = 255 - (x - 768);
+		if ((x >= 1024) && (x <= 1279)) R = x - 1024;
+		if ((x >= 1280) && (x <= 1535)) B = 255 - (x - 1280);
+
+		// Format to hex string with 'ff' (for full opacity) and clamped RGB values
+		std::stringstream ssR, ssG, ssB;
+		ssR << std::setfill('0') << std::setw(HEX_WIDTH) << std::hex << R;
+		ssG << std::setfill('0') << std::setw(HEX_WIDTH) << std::hex << G;
+		ssB << std::setfill('0') << std::setw(HEX_WIDTH) << std::hex << B;
+
+		pinHexIn = "ff" + ssR.str() + ssG.str() + ssB.str(); // Combining into a final hex color string
+
+		return gmpi::MP_OK;
+	}
+
+	void calcDimensionsBg(Point& centerBg, float& radiusBg, float& thicknessBg) {
+		auto r = getRect();
+		centerBg = Point((r.left + r.right) * 0.5f, (r.top + r.bottom) * 0.5f);
+		radiusBg = min(r.getWidth(), r.getHeight()) * 0.4f; // Background radius
+		thicknessBg = radiusBg * THICKNESS_MULTIPLIER; // Background thickness
+	}
+
+	void calcDimensions(Point& center, float& radius, float& thickness) 
+	{
+		auto r = getRect();
+		float mult = circleSize * CIRCLE_SIZE_MULTIPLIER;
+		center = Point((r.left + r.right) * 0.5f, (r.top + r.bottom) * 0.5f);
+		radius = min(r.getWidth(), r.getHeight()) * mult; // Circle radius
+		thickness = radius * THICKNESS_MULTIPLIER;
+	}
+
+	int32_t MP_STDCALL OnRender(GmpiDrawing_API::IMpDeviceContext* drawingContext) override {
+		Graphics g(drawingContext);
 		Point center;
-		float radius;
-		float thickness;
+		float radiusBg, thicknessBg;
+		calcDimensionsBg(center, radiusBg, thicknessBg);
+
+		float radius, thickness;
 		calcDimensions(center, radius, thickness);
 
-		auto brushForeground = g.CreateSolidColorBrush(Color::FromHexString(pinForeground));
-		auto brushBackground = g.CreateSolidColorBrush(Color::FromHexString(pinBackground));
+		auto brushLine = g.CreateSolidColorBrush(Color::FromHexString(pinForeground));
+		auto gradientBrushBg = g.CreateRadialGradientBrush(Color::FromHexString(pinHex), Color::Black, center, radiusBg);
+		auto gradientBrush = g.CreateRadialGradientBrush(Color::FromHexString(pinHex), Color::Black, center, radius);
 
-		const float startAngle = 35.0f; // angle between "straight-down" and start of arc. In degrees.
-		const float startAngleRadians = startAngle * M_PI / 180.f; // angle between "straight-down" and start of arc. In degrees.
-		const float quarterTurnClockwise = M_PI * 0.5f;
+		const float startAngle = pinStartAngle; // Angle between "straight-down" and start of arc
+		const float startAngleRadians = startAngle * (3.14159265358979323846f / 180.f); // Convert to radians
+		const float quarterTurnClockwise = 3.14159265358979323846f * 0.5f;
 
+		// Create stroke style with cap style
 		StrokeStyleProperties strokeStyleProperties;
-		strokeStyleProperties.setCapStyle(CapStyle::Round);
-		strokeStyleProperties.setLineJoin(LineJoin::Round);
+		CapStyle arr[] = { CapStyle::Flat, CapStyle::Square, CapStyle::Round, CapStyle::Triangle };
+		strokeStyleProperties.setCapStyle(arr[pinStyle.getValue()]);
+
 		auto strokeStyle = g.GetFactory().CreateStrokeStyle(strokeStyleProperties);
 
 		Point startPoint(center.x + radius * cosf(quarterTurnClockwise + startAngleRadians), center.y + radius * sinf(quarterTurnClockwise + startAngleRadians));
-		Point midPoint(center.x, center.y - radius);
-		float sweepAngle = (M_PI * 2.0f - startAngleRadians * 2.0f);
-		Point endPoint(center.x + radius * cosf(quarterTurnClockwise + startAngleRadians + sweepAngle), center.y + radius * sinf(quarterTurnClockwise + startAngleRadians + sweepAngle));
+		Point midPoint(center.x + radius, center.y - radius);
 
-		float nomalised = pinAnimationPosition;
-		sweepAngle = nomalised * (static_cast<float>(M_PI) * 2.0f - startAngleRadians * 2.0f);
+		float nominalized = pinAnimationPosition;
+		float sweepAngle = nominalized * (static_cast<float>(3.14159265358979323846) * 2.0f - startAngleRadians * 2.0f);
 		Point movingPoint(center.x + radius * cosf(quarterTurnClockwise + startAngleRadians + sweepAngle), center.y + radius * sinf(quarterTurnClockwise + startAngleRadians + sweepAngle));
 
-		Size circleSize(radius, radius);
-
-		// Background circle.
-		{
-			g.FillCircle(center, radius + thickness * 0.5f, brushBackground);
-		}
-
-		// Line.
-		{
-			g.DrawLine(center, movingPoint, brushForeground, thickness, strokeStyle);
-		}
+		// Draw background circle
+		g.FillCircle(center, radiusBg + thicknessBg * .5f, gradientBrushBg);
+		// Draw the top circle
+		g.FillCircle(center, radius + thickness * .5f, gradientBrush);
+		// Draw the line
+		g.DrawLine(center, movingPoint, brushLine, pinLineThickness, strokeStyle);
 
 		return gmpi::MP_OK;
 	}
