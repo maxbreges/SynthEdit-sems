04d5aa6dc398b8fb88c9d43ccb698edcecc2d185
Max <mbreges@gmail.com>
2026-08-11 11:27:25 +0300

    Update FolderListGui.cpp

--- FolderList/FolderListGui.cpp
+++ FolderList/FolderListGui.cpp
@@ -69,18 +69,6 @@ class FolderListGui final : public SeGuiInvisibleBase
         }
     }
 
-    // Conversion functions for UTF-8 and wstring
-    std::string wstring_to_utf8(const std::wstring& wstr)
-    {
-        static std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-        return conv.to_bytes(wstr);
-    }
-    std::wstring utf8_to_wstring(const std::string& str)
-    {
-        static std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-        return conv.from_bytes(str);
-    }
-
     StringGuiPin pinFilePath;
     IntGuiPin pinChoice;//index
     StringGuiPin pinItemList;  
