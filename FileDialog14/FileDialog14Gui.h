89d7e089786581e9cacea67c379ee3066251c5ce
Max <mbreges@gmail.com>
2026-09-07 12:26:18 +0300

    File Dialog 10.14

---
+++ FileDialog14/FileDialog14Gui.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef FILEDIALOG14GUI_H_INCLUDED
+#define FILEDIALOG14GUI_H_INCLUDED
+
+#include "../se_sdk3/mp_sdk_gui2.h"
+#include "../se_sdk3/mp_gui.h"
+
+class FileDialogGui : public SeGuiInvisibleBase
+{
+	bool m_prev_trigger;
+	GmpiGui::FileDialog nativeFileDialog;
+
+public:
+	FileDialogGui();
+
+	// overrides.
+
+private:
+	void onSetTrigger();
+	void OnFileDialogComplete(int32_t result);
+	std::string getDefaultFolder(std::wstring extension);
+
+	StringGuiPin pinFileName;
+	StringGuiPin pinFileExtension;
+	BoolGuiPin pinTrigger;
+	BoolGuiPin pinSaveMode;
+};
+
+#endif
+
+
