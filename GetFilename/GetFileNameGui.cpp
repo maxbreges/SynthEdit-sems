e3d0731ee6a97c24a2678b08087e45162cbc46dd
Max <mbreges@gmail.com>
2026-09-07 21:43:50 +0300

    Get Filename extension tolower

--- GetFilename/GetFileNameGui.cpp
+++ GetFilename/GetFileNameGui.cpp
@@ -4,22 +4,29 @@ using namespace gmpi;
 
 class GetFilenameGui final : public SeGuiInvisibleBase
 {
-    std::string get_directory(const std::string& path) {
-        size_t found = path.find_last_of("/\\");
-        if (found == std::string::npos) return "";
-        return path.substr(0, found + 1);
+    std::string get_directory(const std::string& path)
+    {
+        size_t sepPos = path.find_last_of("/\\");
+        if (sepPos != std::string::npos)
+            return path.substr(0, sepPos+1);
+        return "";
     }
 
-    std::string get_filename(const std::string& path) {
-        size_t found = path.find_last_of("/\\");
-        if (found == std::string::npos) return "";
-        return path.substr(found + 1);
+    std::string get_filename(const std::string& name)
+    {
+        size_t sepPos = name.find_last_of("/\\");
+        if (sepPos != std::string::npos)
+            return name.substr(sepPos + 1);
+        return name;
     }
 
-    std::string get_extension(const std::string& p_filename)
+    std::string get_extension(std::string& p_filename)
     {
         size_t found = p_filename.find_last_of(".");
-        if (found == std::string::npos) return "";
+        if (found == std::string::npos) return p_filename.substr(found);
+        // Convert extension to lowercase
+        std::transform(p_filename.begin(), p_filename.end(), p_filename.begin(),
+            [](unsigned char c) { return std::tolower(c); });
         return p_filename.substr(found);
     }
 
@@ -53,8 +60,17 @@ public:
     {
         initializePin(pinFilePathIn, static_cast<MpGuiBaseMemberPtr2>(&GetFilenameGui::onSetFilePathIn));
         initializePin(pinDirectory);
-        initializePin(pinFilename);
+        initializePin(pinFilename, static_cast<MpGuiBaseMemberPtr2>(&GetFilenameGui::onSetFileName));
         initializePin(pinExt);
+    }
+    void onSetFileName()
+    {
+        std::string Name = pinFilename;
+        std::string Directory = pinDirectory;
+        std::string Extension = pinExt;
+        std::string fullPath = Directory + Name + Extension;
+
+        pinFilePathIn = fullPath;
     }
 };
 
