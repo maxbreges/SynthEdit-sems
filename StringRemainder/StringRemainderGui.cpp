c568b8d043413c27b5a66b8cf4e52a4f44e88aae
Max <mbreges@gmail.com>
2026-09-15 22:57:42 +0300

    string remainder update

--- StringRemainder/StringRemainderGui.cpp
+++ StringRemainder/StringRemainderGui.cpp
@@ -3,48 +3,61 @@
 REGISTER_GUI_PLUGIN(StringRemainderGui, L"My StringRemainder");
 
 StringRemainderGui::StringRemainderGui (IMpUnknown* host) : MpGuiBase(host)
-
 	{
 		initializePin( pinString1, static_cast<MpGuiBaseMemberPtr>(&StringRemainderGui::onSetString1) );
 		initializePin( pinString2, static_cast<MpGuiBaseMemberPtr>(&StringRemainderGui::onSetString2) );
 		initializePin( pinStringOut );
-		initializePin(pinRelativePath, static_cast<MpGuiBaseMemberPtr>(&StringRemainderGui::onSetString1));
-		initializePin(pinDebug);
-	}
-
-	void StringRemainderGui::onSetString1()
-	{
-		s1 = pinString1; //App Directory
-	}
-
-	void StringRemainderGui::onSetString2()
-	{
-		s2 = pinString2; //User Path
-		stringRemainder();
+		initializePin(pinRelativePath, static_cast<MpGuiBaseMemberPtr>(&StringRemainderGui::onSetRelativePath));
+		initializePin(pinDebug);		
 	}
 
 	int32_t StringRemainderGui::receiveMessageFromAudio(int32_t id, int32_t size, void* messageData)
 	{
 		if (id == 116599)
-		stringRemainder();
+			//pinDebug = "from Audio";
+			onSetString1();		
 		return gmpi::MP_OK;
 	}
 
+	void StringRemainderGui::onSetString1()
+	{
+		//pinDebug = "onSetString1";
+		s1 = pinString1; //App Directory	
+		onSetString2();
+	}
+
+	void StringRemainderGui::onSetString2()
+	{
+		s2 = pinString2; //User Path
+
+		stringRemainder();	
+	}
+	std::string relativePath;
 	void StringRemainderGui::stringRemainder()
 	{
-
 		// Check if s1 is a prefix of s2
-
 		if (s2.compare(0, s1.size(), s1) == 0) {
 			// Output the remainder
-			std::string remainder = s2.substr(s1.size());
+			std::string remainder = s2.substr(s1.size());			
+			if (remainder == s2)
+			{
+				s1 = pinString1;
+				pinStringOut = s1 + std::string(pinRelativePath);
+				return;
+			}
 			pinRelativePath = remainder;
-			pinStringOut = s1 + remainder; //relative path
-			pinDebug = "Check if s1 is a prefix of s2\n" + s1 + "\n" + s2 + "\n" + remainder;
+			onSetRelativePath();
+			pinStringOut = s1 + relativePath; //relative path
+			pinDebug = "Check if s1 is a prefix of s2\n" + std::string("String1  ") + s1 + "\n" + "String2  " + s2 + "\n" + remainder;
 		}
-		else {
-			pinRelativePath = "";
+		else 
+		{
 			pinStringOut = s2;
 			pinDebug = "else part";
 		}		
+	}
+	
+	void StringRemainderGui::onSetRelativePath()
+	{
+		relativePath = pinRelativePath;
 	}
