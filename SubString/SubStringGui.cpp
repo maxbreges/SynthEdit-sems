e445da87011811a91625d7fcf08b8762e0e291cc
Max <mbreges@gmail.com>
2026-09-26 16:11:05 +0300

    substring

--- SubString/SubStringGui.cpp
+++ SubString/SubStringGui.cpp
@@ -8,48 +8,57 @@ class SubStringGui final : public SeGuiInvisibleBase
     std::string s2;
     std::string subString;
 
-    void onSetS2()
-    {
-        s2 = pinString2;
-        if(s1.empty())
-        {
-            pinString1 = s2 + subString;
-        }        
-    }
-
-    void onSetSubString()
+    void onSetS1()
     {
         s1 = pinString1;
         s2 = pinString2;
+
+        if (s1.empty())
+        {
+            onSetResetIn();            
+        }
 
         if (s1.compare(0, s2.size(), s2) == 0)
         {
             // Output the remainder
             subString = s1.substr(s2.size());
-        }
-        else
-        {
-            subString = s1;
-        }
 
-        pinSubString = subString;
+            pinSubString = s2 + subString;
+        }
+        if (s1.compare(0, s2.size(), s2) != 0)
+        {
+            pinSubString = s1;
+        }
+    }
+
+    void onSetS2()
+    {
+        if(s1.empty())
+        {
+          pinString1 = s2 + subString;
+        }
     }
 
     StringGuiPin pinString1;
     StringGuiPin pinString2;
     StringGuiPin pinSubString;
+    BoolGuiPin pinResetIn;
 
 public:
     SubStringGui()
     {
-        initializePin(pinString1, static_cast<MpGuiBaseMemberPtr2>(&SubStringGui::onSetSubString));
+        initializePin(pinString1, static_cast<MpGuiBaseMemberPtr2>(&SubStringGui::onSetS1));
         initializePin(pinString2, static_cast<MpGuiBaseMemberPtr2>(&SubStringGui::onSetS2));
         initializePin(pinSubString, static_cast<MpGuiBaseMemberPtr2>(&SubStringGui::onSetPatchValue));
+        initializePin(pinResetIn, static_cast<MpGuiBaseMemberPtr2>(&SubStringGui::onSetResetIn));
     }
     void onSetPatchValue()
     {
-        subString = pinSubString;
-        pinString1 = s2 + subString;
+    }
+
+    void onSetResetIn()
+    {
+        pinString1 = pinSubString;
     }
 };
 
